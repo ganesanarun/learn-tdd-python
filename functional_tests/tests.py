338c4c9cdd6e2018d795b43c06b0791bed84559f
ganesan.arunachalam <ganesan.arunachalam@gojek.com>
2021-05-18 16:51:58 +0530

    Use manage.py for running FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,13 @@
-import time
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
+import time
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
@@ -12,13 +15,21 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def check_for_item_in_the_list_table(self, item_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(item_text, [row.text for row in rows])
+    def wait_for_item_in_the_list_table(self, item_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(item_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         self.assertIn("To-do", self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -29,19 +40,14 @@ class NewVisitorTest(unittest.TestCase):
                          input_box.get_attribute('placeholder'))
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.wait_for_item_in_the_list_table('1: Buy peacock feathers')
 
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual('Enter a to-do item',
                          input_box.get_attribute('placeholder'))
         input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_item_in_the_list_table('1: Buy peacock feathers')
-        self.check_for_item_in_the_list_table(
+        self.wait_for_item_in_the_list_table('1: Buy peacock feathers')
+        self.wait_for_item_in_the_list_table(
             '2: Use peacock feathers to make a fly')
-
-
-if __name__ == "__main__":
-    unittest.main()
