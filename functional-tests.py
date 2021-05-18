529f08b9d896df947f5d5741df27b902ccce8e99
ganesan.arunachalam <ganesan.arunachalam@gojek.com>
2021-05-18 14:54:50 +0530

    Model for list Items and associated migration

--- functional-tests.py
+++ functional-tests.py
@@ -1,4 +1,6 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -10,10 +12,35 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_item_in_the_list_table(self, item_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(item_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get("http://localhost:8000")
 
         self.assertIn("To-do", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual('Enter a to-do item',
+                         input_box.get_attribute('placeholder'))
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual('Enter a to-do item',
+                         input_box.get_attribute('placeholder'))
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_item_in_the_list_table('1: Buy peacock feathers')
+        self.check_for_item_in_the_list_table(
+            '2: Use peacock feathers to make a fly')
 
 
 if __name__ == "__main__":
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
-from lists.views import home_page
-from django.urls import resolve
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -8,3 +7,28 @@ class HomePageTest(TestCase):
     def test_home_page_that_returns_html_page(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_post_request(self):
+        response = self.client.post('/', {'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode('utf8'))
+        self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        all_items = Item.objects.all()
+        self.assertEqual(all_items.count(), 2)
+
+        first_saved_item = all_items[0]
+        second_saved_item = all_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
